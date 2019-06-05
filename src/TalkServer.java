80552c31803d513847d6bd2918b32bf3ba110574
javabaiyunjing <763132268@qq.com>
2019-06-05 23:07:20 +0800

    kdhkahdk

--- src/TalkServer.java
+++ src/TalkServer.java
@@ -35,5 +35,3 @@ public class TalkServer {
         }catch (Exception e){System.out.println("Error"+ e);}
     }
     }
-
-
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,8 +1,8 @@
 import java.net.*;
 public class UDPServer {
     public static void main (String agrs [])throws  Exception{
-        byte buf[] = new byte[1024];
-        DatagramPacket dp = new DatagramPacket(buf,buf.length);
+        byte buf[] = new byte[1024];//声明一个buf[]数组，容量为1024个字节
+        DatagramPacket dp = new DatagramPacket(buf,buf.length);//创建一个数据包，
         DatagramSocket ds = new DatagramSocket(5788);
         while(true) {
             ds.receive(dp);

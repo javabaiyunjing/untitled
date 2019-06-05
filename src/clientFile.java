60ca6e58b55f08ce03aad0dd8b7558ac1c9ee2e0
javabaiyunjing <763132268@qq.com>
2019-06-05 23:06:55 +0800

    kdhkahdk

--- src/clientFile.java
+++ src/clientFile.java
@@ -2,18 +2,16 @@ import java.io.*;
 import java.net.Socket;
 public class clientFile {
     public static void main (String agrs [] )throws Exception {
-        Socket s = new Socket("127.0.0.1",6666);
-        File f = new File("C:/哇哈哈6.txt");
+        Socket s = new Socket("127.0.0.1", 6666);
+        File f = new File("C:/哇哈哈2.txt");
         InputStream is = s.getInputStream();
-        //从client端的向外输出
         FileOutputStream fos = new FileOutputStream(f);
-        byte buf [] = new byte[100];
-        int i = 0;
-        while ((i = is.read(buf))!=-1) {
-            fos.write(buf, 0, i);
+        byte buf[] = new byte[100];
+        int i = 0 ;
+        while ((i = is.read(buf))!=-1){
+            fos.write(buf,0,i);
         }
         is.close();
         fos.close();
-
     }
- }
+    }

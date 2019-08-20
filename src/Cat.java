f89bd0081ea03f28aa8e84addb090946d71312f5
javabaiyunjing <763132268@qq.com>
2019-08-20 22:28:06 +0800

    kdhkahdk

---
+++ src/Cat.java
@@ -0,0 +1,7 @@
+public class Cat {
+    public static String ttt = null;
+
+    public Cat(String name){
+        this.ttt = name;
+    }
+}
---
+++ src/CatRunner.java
@@ -0,0 +1,17 @@
+public class CatRunner {
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("111");
+
+        System.out.println(Cat.ttt);
+
+        Cat cat2  = new Cat("222");
+
+        System.out.println(Cat.ttt);
+
+
+        System.out.println("111");
+
+    }
+
+}
